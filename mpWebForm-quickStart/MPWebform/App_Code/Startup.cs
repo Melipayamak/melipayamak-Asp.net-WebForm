4346ff333ed44689ebf8bcdbb29e084c34ad8ada
Amirhossein Mehrvarzi <36813422+triggergroup@users.noreply.github.com>
2019-05-26 00:00:50 +0430

    Add files via upload

---
+++ mpWebForm-quickStart/MPWebform/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MPWebform.Startup))]
+namespace MPWebform
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ mpWebForm-quickStart/MPWebform/Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using ServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+	protected void onSoapClick(object sender, EventArgs e)
+	{
+		const string username = "username";
+		const string password = "password";
+		const string from = "5000...";
+		const string to = "09123456789";
+		const string text = "تست وب سرویس ملی پیامک";
+		const bool isFlash = false;
+		SendSoapClient soapClient = new SendSoapClient();
+		var result = soapClient.SendSimpleSMS2(username, password, to, from, text, isFlash);
+	}
+
+	protected void onRestClick(object sender, EventArgs e)
+	{
+		const string username = "username";
+		const string password = "password";
+		const string from = "5000...";
+		const string to = "09123456789";
+		const string text = "تست وب سرویس ملی پیامک";
+		const bool isFlash = false;
+		RestClient restClient = new RestClient(username, password);
+		var result = restClient.Send(to, from, text, isFlash);
+	}
+}
